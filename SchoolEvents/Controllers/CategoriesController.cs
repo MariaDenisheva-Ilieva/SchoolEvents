fcd010351f03f48247036df121b716a6f947efbb
Maria Denisheva-Ilieva <maria_denisheva73@abv.bg>
2026-09-26 06:44:51 +0300

    Добавяне на страници за категории и организатори

---
+++ SchoolEvents/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolEvents.Data;
+
+namespace SchoolEvents.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly SchoolEventsDbContext _context;
+
+        public CategoriesController(SchoolEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // Показва всички категории
+        public async Task<IActionResult> Index()
+        {
+            var categories = await _context.Categories
+                .ToListAsync();
+
+            return View(categories);
+        }
+    }
+}
---
+++ SchoolEvents/Controllers/OrganizersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SchoolEvents.Data;
+
+namespace SchoolEvents.Controllers
+{
+    public class OrganizersController : Controller
+    {
+        private readonly SchoolEventsDbContext _context;
+
+        public OrganizersController(SchoolEventsDbContext context)
+        {
+            _context = context;
+        }
+
+        // Показва всички организатори
+        public async Task<IActionResult> Index()
+        {
+            var organizers = await _context.Organizers
+                .ToListAsync();
+
+            return View(organizers);
+        }
+    }
+}
